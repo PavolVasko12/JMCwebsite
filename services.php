d77a58369019aa3de119efc2eb4c013d5a52a65b
Alexandre Lissac <alex36330@gmail.com>
2018-01-30 21:11:25 +0000

    Section Service amends

--- services.php
+++ services.php
@@ -1,8 +1,26 @@
 <div id="servicesSection">
-  <h1 class="titleSection" id="">SERVICES</h1>
   <div id="servicesHolder">
-    <div class="oneServiceContent"></div>
-    <div class="oneServiceContent"></div>
-    <div class="oneServiceContent"></div>
+    <h1 class="titleSection" id="serviceTitle">SERVICES</h1>
+    <div class="oneServiceContent">
+      <h2 class="secondTitleService">CONSULTING</h2>
+      <object>
+        <embed src="content/mountainIntro.svg" class="iconService">
+      </object>
+      <h2 class="greenTextSection">Pathfinding</h2>
+    </div>
+    <div class="oneServiceContent">
+      <h2 class="secondTitleService">PROJECTS</h2>
+      <object>
+        <embed src="content/buildingIntro.svg" class="iconService">
+      </object>
+      <h2 class="greenTextSection">Activation</h2>
+    </div>
+    <div class="oneServiceContent">
+      <h2 class="secondTitleService">EVENTS</h2>
+      <object>
+        <embed src="content/flowerIntro.svg" class="iconService">
+      </object>
+      <h2 class="greenTextSection">Experiences</h2>
+    </div>
   </div>
 </div>

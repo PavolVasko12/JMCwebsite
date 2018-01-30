e62960ecd6d53e549bb5c2255ea64885587bf6f2
Alexandre Lissac <alex36330@gmail.com>
2018-01-30 01:53:55 +0000

    Starting Project
    Heading Section Done
    Introduction Section Done
    Folder renaming (Website content into content)
    Svg added
    Font added

--- index.php
+++ index.php
@@ -8,30 +8,39 @@
 
   <body>
     <div id="bodyWrapper">
-	<!-- ------------------ NAVIGATION FROM HERE -------------------- -->
-    <?php require_once('navigation.php'); ?>
+    	<!-- ------------------ NAVIGATION FROM HERE -------------------- -->
+        <?php require_once('navigation.php'); ?>
 
-	  <!-- Hamburger icon -->
-	  <div id="hamburgerMenuHolder">
-	    <div id="barOne"></div>
-	    <div id="barTwo"></div>
-	    <div id="barThree"></div>
-	  </div>
+    	  <!-- Hamburger icon -->
+    	  <div id="hamburgerMenuHolder">
+    	    <div id="barOne"></div>
+    	    <div id="barTwo"></div>
+    	    <div id="barThree"></div>
+    	  </div>
 
-	<!-- ---------------- INTRODUCTION FROM HERE ----------------- -->
-	<?php require_once('introduction.php'); ?>
+        <div id="homepage">
+          <div id="homepageContentHolder">
+            <p id="topTextHomepage">JMC</p>
+            <div id="blackHolderHomepage">
+              <p id="bottomTextHomepage">PROJECT CONSULTANCY</p>
+            </div>
+          </div>
+        </div>
 
-	<!-- ---------------- ABOUT US FROM HERE ----------------- -->
-	<?php require_once('aboutUs.php'); ?>
+    	<!-- ---------------- INTRODUCTION FROM HERE ----------------- -->
+    	<?php require_once('introduction.php'); ?>
 
-	<!-- ---------------- SERVICES FROM HERE ----------------- -->
-	<?php require_once('services.php'); ?>
+    	<!-- ---------------- ABOUT US FROM HERE ----------------- -->
+    	<?php require_once('aboutUs.php'); ?>
 
-	<!-- ---------------- PROJETS FROM HERE ----------------- -->
-	<?php require_once('projects.php'); ?>
+    	<!-- ---------------- SERVICES FROM HERE ----------------- -->
+    	<?php require_once('services.php'); ?>
 
-	<!-- ---------------- CONTACT FROM HERE ----------------- -->
-	<?php require_once('contact.php'); ?>
+    	<!-- ---------------- PROJETS FROM HERE ----------------- -->
+    	<?php require_once('projects.php'); ?>
+
+    	<!-- ---------------- CONTACT FROM HERE ----------------- -->
+    	<?php require_once('contact.php'); ?>
 
 <!-- End of the body wrraper -->
 </div>
@@ -39,6 +48,6 @@
 
 <!-- ------------------ SCRIPTS FROM HERE -------------------- -->
     <?php require_once('scripts.php'); ?>
-    
+
     </body>
 </html>
---
+++ introduction.php
@@ -0,0 +1,22 @@
+<div id="introSection">
+  <div id="leftHolderIntro">
+  </div>
+  <div id="rightHolderIntro">
+    <div id="rightContentHolderIntro">
+      <h1 class="titleSection">Hello and Welcome</h1>
+      <h2 id="secondTitleIntro">We help our clients design and build venues and unique experiences.</h2>
+      <div id="iconsHolderIntro">
+        <object>
+          <embed src="content/mountainIntro.svg" class="iconIntro">
+        </object>
+        <object>
+          <embed src="content/buildingIntro.svg" class="iconIntro">
+        </object>
+        <object>
+          <embed src="content/flowerIntro.svg" class="iconIntro">
+        </object>
+      </div>
+      <p id="bottomTextIntro">From years spent in the field we have gained vast practical expertise, creative know and a great network of fellow consultants and contractors. We specialise in bringing people together to create something from nothing.</p>
+    </div>
+  </div>
+</div>
